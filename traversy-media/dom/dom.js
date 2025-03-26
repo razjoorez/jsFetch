776ec59fd07dacc2c72e617e729e76a06572f884
reza razjoo <reza.razjoo@yahoo.com>
2025-03-26 23:10:48 +0000

    dom

---
+++ traversy-media/dom/dom.js
@@ -0,0 +1,35 @@
+//console.dir(document);
+
+
+//examine the document object
+
+
+// console.log(document.domain);
+// console.log(document.URL)
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.all);
+// console.log(document.all[10]);
+
+//document.all[9].textContent = 'hello'
+
+
+// console.log(document.forms[0]);
+// console.log(document.links);
+
+
+//console.log(document.images);
+
+// var headerTitle = document.getElementById('header-title')
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'GoodBye';
+// headerTitle.innerHTML = '<h3>Hello</h3>'
+
+
+
+
+
+
+
+
