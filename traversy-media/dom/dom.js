90a4cf2fde3fbd817b42f25e5d37625c72e50d0d
reza razjoo <reza.razjoo@yahoo.com>
2025-03-28 17:14:00 +0000

    dom part 1

--- traversy-media/dom/dom.js
+++ traversy-media/dom/dom.js
@@ -40,6 +40,63 @@ for(var i=0;i<items.length; i++) {
     items[i].style.backgroundColor = '#f4f4f4';
 }
 
+//Elements by tagName
+
+
+var li = document.getElementsByTagName('li');
+
+for(var i=0; i<li.length; i++) {
+    li[i].style.backgroundColor = '#f4f4f4';
+}
+
+
+//Query selector
+
+var header = document.querySelector('#main-header');
+header.style.borderBottom = '5px solid red';
+
+var input = document.querySelector('input');
+input.value = 'Hello';
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = 'Send';
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'green';
+
+
+//QuerySelector ALL
+
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i=0; i<odd.length; i++) {
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
